8b5d164fb9445f224813e134ef6157d8be37f5f7
volt901 <volt901@icloud.com>
2023-06-09 22:01:42 +0500

    Initial Commit

---
+++ todayCalendar/todayCalendarApp.swift
@@ -0,0 +1,17 @@
+//
+//  todayCalendarApp.swift
+//  todayCalendar
+//
+//  Created by Владимир Ширяев on 09.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct todayCalendarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
